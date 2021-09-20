ae5b867c86f32644535e67fa3fac6a372836d182
vclbuff <vclbuff@gmail.com>
2021-09-20 19:42:30 +0900

    wip

---
+++ components/organisms/RoomList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="room__list">
+    <li
+      v-for="item in items"
+      :key="item.username"
+      class="room__item"
+    >
+      {{ item.username }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      requried: true,
+      default: () => { return [] }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.room__list {
+  margin: 0;
+}
+
+.room__item {
+  border-bottom: 1px solid $cGray;
+  padding: $basePadding;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,85 @@
 <template>
-  <div>
-    main
+  <div class="dm">
+    <h1>
+      Messages
+    </h1>
+    <div class="dm__inner">
+      <OrganismsRoomList
+        :items="dmList"
+        class="dm__room"
+      />
+      <OrganismsRoomMessages
+        class="dm__messages"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      dmList: [],
+      message: []
+    }
+  },
+  fetch () {
+    this.dmList = [
+      {
+        username: 'usera',
+        lastMessage: 'Hello'
+      },
+      {
+        username: 'userb',
+        lastMessage: 'Hello'
+      },
+      {
+        username: 'userb',
+        lastMessage: 'Hello'
+      }
+    ]
+    this.messages = [
+      {
+        username: 'usera',
+        postedAt: new Date().getTime(),
+        message: 'Hello'
+      },
+      {
+        username: 'usera',
+        postedAt: new Date().getTime() + 1,
+        message: 'World'
+      },
+      {
+        username: 'userb',
+        postedAt: new Date().getTime(),
+        message: 'Hello'
+      },
+      {
+        username: 'userc',
+        postedAt: new Date().getTime(),
+        message: 'Hello'
+      }
+    ]
+  }
+}
 </script>
 
 <style lang="scss">
-.header {
-  color: $gray;
+.dm__inner {
+  display: flex;
+  min-height: calc(100vh - #{$headerHeight} - #{$footerHeight});
+  border-top: 1px solid $cGray;
+}
+
+.dm__room {
+  padding: $basePadding;
+  border-right: 1px solid $cGray;
+  width: 240px;
+  flex-shrink: 0;
+}
+
+.dm__messages {
+  padding: $basePadding;
+  width: 100%;
 }
 </style>
